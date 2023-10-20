2c3e62f49281764e85c8635b9122b3e2cab03d27
Kyryl Koblyk <nytshow612@gmail.com>
2023-10-20 21:26:51 +0300

    Changed MessageCopyBox size

--- PowerLearnCreator/Forms/MessageCopyBox.Designer.cs
+++ PowerLearnCreator/Forms/MessageCopyBox.Designer.cs
@@ -45,20 +45,22 @@
             this.tableLayoutPanel.Controls.Add(this.lblMessage, 0, 0);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
-            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.tableLayoutPanel.RowCount = 2;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.75343F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.24657F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(633, 176);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(456, 249);
             this.tableLayoutPanel.TabIndex = 0;
             // 
             // btnCopy
             // 
             this.btnCopy.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnCopy.Location = new System.Drawing.Point(8, 117);
+            this.btnCopy.Location = new System.Drawing.Point(12, 164);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnCopy.Name = "btnCopy";
-            this.btnCopy.Size = new System.Drawing.Size(305, 51);
+            this.btnCopy.Size = new System.Drawing.Size(212, 74);
             this.btnCopy.TabIndex = 0;
             this.btnCopy.Text = "Copy";
             this.btnCopy.UseVisualStyleBackColor = true;
@@ -68,11 +70,12 @@
             // 
             this.btnClancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClancel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnClancel.Location = new System.Drawing.Point(319, 117);
+            this.btnClancel.Location = new System.Drawing.Point(232, 164);
+            this.btnClancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnClancel.Name = "btnClancel";
-            this.btnClancel.Size = new System.Drawing.Size(306, 51);
+            this.btnClancel.Size = new System.Drawing.Size(212, 74);
             this.btnClancel.TabIndex = 1;
-            this.btnClancel.Text = "Cancel";
+            this.btnClancel.Text = "Close";
             this.btnClancel.UseVisualStyleBackColor = true;
             this.btnClancel.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -81,9 +84,10 @@
             this.lblMessage.AutoSize = true;
             this.tableLayoutPanel.SetColumnSpan(this.lblMessage, 2);
             this.lblMessage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblMessage.Location = new System.Drawing.Point(8, 5);
+            this.lblMessage.Location = new System.Drawing.Point(12, 8);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(617, 109);
+            this.lblMessage.Size = new System.Drawing.Size(432, 153);
             this.lblMessage.TabIndex = 4;
             this.lblMessage.Text = "Message";
             this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -91,13 +95,16 @@
             // MessageCopyBox
             // 
             this.AcceptButton = this.btnCopy;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnClancel;
-            this.ClientSize = new System.Drawing.Size(633, 176);
+            this.ClientSize = new System.Drawing.Size(456, 249);
             this.Controls.Add(this.tableLayoutPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "MessageCopyBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Information to copy";
             this.tableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.PerformLayout();
