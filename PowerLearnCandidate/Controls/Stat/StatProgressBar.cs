45e042acbcf5f5e99fbe25719f0457f3d38885b4
Kyryl Koblyk <nytshow612@gmail.com>
2023-10-30 20:05:35 +0200

    Start changing UI of test result

---
+++ PowerLearnCandidate/Controls/Stat/StatProgressBar.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerLearnCandidate.Controls.Stat
+{
+    public class StatProgressBar : Control
+    {
+        private float _value;
+        private string leftText;
+        private string rightText;
+        private Color leftColor;
+        private Color rightColor;
+
+        public float Value
+        {
+            get => _value; set
+            {
+                if (_value == value)
+                {
+                    return;
+                }
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Value is out of range!");
+                }
+                _value = value;
+                Invalidate();
+            }
+        }
+        public string LeftText
+        {
+            get => leftText; set
+            {
+                if (leftText == value)
+                {
+                    return;
+                }
+                leftText = value;
+                Invalidate();
+            }
+        }
+        public string RightText
+        {
+            get => rightText; set
+            {
+                if (rightText == value)
+                {
+                    return;
+                }
+                rightText = value;
+                Invalidate();
+            }
+        }
+        public Color LeftColor
+        {
+            get => leftColor; set
+            {
+                if (leftColor == value)
+                {
+                    return;
+                }
+                leftColor = value;
+                Invalidate();
+            }
+        }
+        public Color RightColor
+        {
+            get => rightColor; set
+            {
+                if (rightColor == value)
+                {
+                    return;
+                }
+                rightColor = value;
+                Invalidate();
+            }
+        }
+
+        public StatProgressBar()
+        {
+            SetStyle(ControlStyles.UserPaint
+                | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.ResizeRedraw
+                | ControlStyles.SupportsTransparentBackColor, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
--- PowerLearnCandidate/Forms/ResultForm.Designer.cs
+++ PowerLearnCandidate/Forms/ResultForm.Designer.cs
@@ -40,10 +40,9 @@
             // lblTestName
             // 
             this.lblTestName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTestName.Location = new System.Drawing.Point(23, 11);
-            this.lblTestName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTestName.Location = new System.Drawing.Point(17, 9);
             this.lblTestName.Name = "lblTestName";
-            this.lblTestName.Size = new System.Drawing.Size(643, 31);
+            this.lblTestName.Size = new System.Drawing.Size(482, 25);
             this.lblTestName.TabIndex = 0;
             this.lblTestName.Text = "Name";
             this.lblTestName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -51,10 +50,9 @@
             // lblScore
             // 
             this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblScore.Location = new System.Drawing.Point(23, 58);
-            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScore.Location = new System.Drawing.Point(17, 47);
             this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(643, 31);
+            this.lblScore.Size = new System.Drawing.Size(482, 25);
             this.lblScore.TabIndex = 1;
             this.lblScore.Text = "Score";
             this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -62,20 +60,18 @@
             // lblMaxScore
             // 
             this.lblMaxScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblMaxScore.Location = new System.Drawing.Point(23, 106);
-            this.lblMaxScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMaxScore.Location = new System.Drawing.Point(17, 86);
             this.lblMaxScore.Name = "lblMaxScore";
-            this.lblMaxScore.Size = new System.Drawing.Size(643, 31);
+            this.lblMaxScore.Size = new System.Drawing.Size(482, 25);
             this.lblMaxScore.TabIndex = 2;
             this.lblMaxScore.Text = "Max Score";
             this.lblMaxScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnResult
             // 
-            this.btnResult.Location = new System.Drawing.Point(289, 278);
-            this.btnResult.Margin = new System.Windows.Forms.Padding(4);
+            this.btnResult.Location = new System.Drawing.Point(217, 226);
             this.btnResult.Name = "btnResult";
-            this.btnResult.Size = new System.Drawing.Size(103, 28);
+            this.btnResult.Size = new System.Drawing.Size(77, 23);
             this.btnResult.TabIndex = 3;
             this.btnResult.Text = "Upload result";
             this.btnResult.UseVisualStyleBackColor = true;
@@ -84,10 +80,9 @@
             // lblTestAuthor
             // 
             this.lblTestAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTestAuthor.Location = new System.Drawing.Point(16, 148);
-            this.lblTestAuthor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTestAuthor.Location = new System.Drawing.Point(12, 120);
             this.lblTestAuthor.Name = "lblTestAuthor";
-            this.lblTestAuthor.Size = new System.Drawing.Size(649, 31);
+            this.lblTestAuthor.Size = new System.Drawing.Size(487, 25);
             this.lblTestAuthor.TabIndex = 4;
             this.lblTestAuthor.Text = "Author";
             this.lblTestAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -95,10 +90,9 @@
             // lblTestType
             // 
             this.lblTestType.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTestType.Location = new System.Drawing.Point(16, 190);
-            this.lblTestType.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTestType.Location = new System.Drawing.Point(12, 154);
             this.lblTestType.Name = "lblTestType";
-            this.lblTestType.Size = new System.Drawing.Size(649, 31);
+            this.lblTestType.Size = new System.Drawing.Size(487, 25);
             this.lblTestType.TabIndex = 5;
             this.lblTestType.Text = "Type";
             this.lblTestType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -106,19 +100,18 @@
             // lblTestDesription
             // 
             this.lblTestDesription.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTestDesription.Location = new System.Drawing.Point(16, 233);
-            this.lblTestDesription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTestDesription.Location = new System.Drawing.Point(12, 189);
             this.lblTestDesription.Name = "lblTestDesription";
-            this.lblTestDesription.Size = new System.Drawing.Size(649, 31);
+            this.lblTestDesription.Size = new System.Drawing.Size(487, 25);
             this.lblTestDesription.TabIndex = 6;
             this.lblTestDesription.Text = "Description";
             this.lblTestDesription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ResultForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 317);
+            this.ClientSize = new System.Drawing.Size(529, 309);
             this.Controls.Add(this.lblTestDesription);
             this.Controls.Add(this.lblTestType);
             this.Controls.Add(this.lblTestAuthor);
@@ -127,7 +120,6 @@
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.lblTestName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "ResultForm";
             this.ResumeLayout(false);
